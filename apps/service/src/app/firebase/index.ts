d03971d4b424a4d0346965f83b714ab606bb2fdf
Jhonathan <nathan3boss@gmail.com>
2023-12-22 16:47:19 -0300

    test: add restore all mocks

--- apps/service/src/app/firebase/index.ts
+++ apps/service/src/app/firebase/index.ts
@@ -1,7 +1,13 @@
 import { env } from '@lumi/env';
 import * as admin from 'firebase-admin';
 
+const config = {
+  production: '@lumi/firebase/firebase.prod.json',
+  test: '@lumi/firebase/firebase.example.json',
+  dev: '@lumi/firebase/firebase.dev.json',
+};
+
 export const firebase = admin.initializeApp({
-  credential: admin.credential.cert(require('@lumi/firebase/firebase.json')),
+  credential: admin.credential.cert(require(config[env.NODE_ENV])),
   storageBucket: env.FIREBASE_STORAGE_BUCKET,
 });
